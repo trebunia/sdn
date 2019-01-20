a0c2cb17e756787841d467d76af3dd0144c7241d
wgorecki <witoldg94@gmail.com>
2019-01-20 11:48:21 +0100

    zmieniony troche skrypt startujacy mininet

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Aby odpalic mininet'a z nasza topologia nalezy wykonac komende jako root (albo z sudo):
 ```bash
-mn --custom sdn.py --topo mytopo --controller=remote,ip=<controller ip>,port=6653
+./sdn.py ControllerIP
 ```
 
-```controller ip``` to localhost jeśli mamy minineta tam gdzie floodlighta, albo adres hosta jeśli jesteśmy na wirtualce mininetowej.
+```ControllerIP``` to localhost jeśli mamy minineta tam gdzie floodlighta, albo adres hosta jeśli jesteśmy na wirtualce mininetowej.
--- sdn.py
+++ sdn.py
@@ -1,40 +1,64 @@
 #!/usr/bin/python
 
 from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+from mininet.node import RemoteController
+from mininet.cli import CLI
+import sys
+import subprocess
+import os.path
 
-class MyTopo( Topo ):
-	"Simple topology example."
-	def __init__( self ):
-		"Create custom topo."
-		# Initialize topology
-		Topo.__init__( self )
-		# Add hosts and switches
-		hosts = [ self.addHost( h ) for h in 'h1', 'h2', 'h3' ]
-                servers = [ self.addHost ( sv ) for sv in 'sv1', 'sv2', 'sv3' ]
-		switches = [ self.addSwitch( s ) for s in 's1', 's2', 's3', 's4', 's5', 's6' ]
-		# Add links
-                # links hosts - switches
-                self.addLink(hosts[0], switches[3])
-                self.addLink(hosts[1], switches[4])
-                self.addLink(hosts[2], switches[5])
+ControllerIP='192.168.56.1'
 
-                # links servers - switches
-                self.addLink(servers[0], switches[0])
-                self.addLink(servers[1], switches[1])
-                self.addLink(servers[2], switches[2])
+class TopoSDN(Topo):
+    def build(self):
+	hosts = [ self.addHost( h ) for h in 'h1', 'h2', 'h3' ]
+	servers = [ self.addHost ( sv ) for sv in 'sv1', 'sv2', 'sv3' ]
+	switches = [ self.addSwitch( s ) for s in 's1', 's2', 's3', 's4', 's5', 's6' ]
+	# Add links
+	# links hosts - switches
+	self.addLink(hosts[0], switches[3])
+	self.addLink(hosts[1], switches[4])
+	self.addLink(hosts[2], switches[5])
+	
+	# links servers - switches
+	self.addLink(servers[0], switches[0])
+	self.addLink(servers[1], switches[1])
+	self.addLink(servers[2], switches[2])
+	
+	# links between switches
+	self.addLink(switches[0], switches[3])
+	self.addLink(switches[0], switches[4])
+	self.addLink(switches[0], switches[5])
+	
+	self.addLink(switches[1], switches[3])
+	self.addLink(switches[1], switches[4])
+	self.addLink(switches[1], switches[5])
+	
+	self.addLink(switches[2], switches[3])
+	self.addLink(switches[2], switches[4])
+	self.addLink(switches[2], switches[5])
 
-                # links between switches
-                self.addLink(switches[0], switches[3])
-                self.addLink(switches[0], switches[4])
-                self.addLink(switches[0], switches[5])
+def simpleTest(controllerip):
+    if not os.path.isfile("./file"):
+        subprocess.call(["dd", "if=/dev/zero", "of=./file", "bs=1M", "count=100"])
+    topo = TopoSDN()
+    net = Mininet(topo, controller=RemoteController( 'c0', ip=controllerip, port=6653 ))
+    net.start()
+    sv1, sv2, sv3 = net.get( 'sv1', 'sv2', 'sv3' )
+    sv1.cmd('python -m SimpleHTTPServer 80 &')
+    sv2.cmd('python -m SimpleHTTPServer 80 &')
+    sv3.cmd('python -m SimpleHTTPServer 80 &')
+    CLI(net)
+    net.stop()
 
-                self.addLink(switches[1], switches[3])
-                self.addLink(switches[1], switches[4])
-                self.addLink(switches[1], switches[5])
+if __name__ == '__main__':
+    # Tell mininet to print useful information
+    if len(sys.argv) == 1:
+        print "How to run ./sdn.py ControllerIP"
+        sys.exit(1)
 
-                self.addLink(switches[2], switches[3])
-                self.addLink(switches[2], switches[4])
-                self.addLink(switches[2], switches[5])
-
-topos = { 'mytopo': ( lambda: MyTopo() ) }
-
+    setLogLevel('info')
+    simpleTest(sys.argv[1])
